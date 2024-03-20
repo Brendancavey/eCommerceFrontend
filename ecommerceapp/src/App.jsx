f8e8768063ed21c779d0a670dc2cee0ea9fd5edb
Brendan <bthoeung@gmail.com>
2024-03-20 11:02:39 -0700

    Add project files.

---
+++ ecommerceapp/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet,
+} from "react-router-dom"
+import Home from "./Pages/Home/Home"
+import Product from "./Pages/Product/Product"
+import Products from "./Pages/Products/Products"
+import NavBar from "./Components/NavBar/NavBar"
+import Footer from "./Components/Footer/Footer"
+import './App.css'
+
+const LayOut = () => {
+    return (
+        <div className='app'>
+            <NavBar />
+            <Outlet/>
+            <Footer/>
+        </div>
+    )
+}
+const router = createBrowserRouter([
+{
+    path: "/",
+    element: <LayOut />,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/products/:id",
+                element: <Products />
+            },
+            {
+                path: "/product/:id",
+                element: <Product />
+            },
+        ]
+},
+])
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+          <RouterProvider router={router } />
+    </>
+  )
+}
+
+export default App
